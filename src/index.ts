b0a7c435ab6a891a29d4cd8eb961e027dd557706
Victor Raúl García García <victor07mejor@gmail.com>
2026-04-08 16:57:40 -0600

    feat: agrega biome al proyecto

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { app } from "@/core/shared/infrastructure/http/server";
 import { env } from "./core/config/env";
 
 app.listen(env.PORT, () => {
-  console.log("SERVER ON PORT " + env.PORT);
+  console.log(`SERVER ON PORT http://localhost:${env.PORT}`);
 });
