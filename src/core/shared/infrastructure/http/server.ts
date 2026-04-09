9e8ba6f6c4565bae16ed23051c386783cddf2235
Victor Raúl García García <victor07mejor@gmail.com>
2026-04-08 18:36:31 -0600

    ref: quita Awilix y refactoriza las dependencias

--- src/core/shared/infrastructure/http/server.ts
+++ src/core/shared/infrastructure/http/server.ts
@@ -1,32 +1,18 @@
 import express from "express";
-import { container } from "@/core/shared/infrastructure/di/container";
-import type { EventBus } from "../../domain/events/EventBus";
-import type { AwilixContainer } from "awilix";
+import { NodeEventBus } from "@/core/shared/infrastructure/events/NodeEventBus";
 import { userRoutes } from "@/modules/user/infrastructure/routes/userRoutes";
 import cors from "cors";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    container?: AwilixContainer;
-  }
-}
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Middleware manual súper sencillo para inyectar el scope en el Request
-app.use((req, res, next) => {
-  req.container = container.createScope();
-  next();
-});
-
 app.use("/api/user", userRoutes);
 
 function bootstrapEvents() {
-  // Sacamos el Event Bus y los Listeners del contenedor de Awilix
-  const eventBus = container.resolve("eventBus") as EventBus;
+  // Instanciamos el Event Bus manualmente
+  const eventBus = new NodeEventBus();
 }
 
 bootstrapEvents();
--- src/modules/user/infrastructure/routes/userRoutes.ts
+++ src/modules/user/infrastructure/routes/userRoutes.ts
@@ -1,23 +1,45 @@
 import { Router } from "express";
-import { makeInvoker } from "awilix-express";
+
+// Infraestructura
+import { PrismaUserRepository } from "../repository/PrismaUserRepository";
+import { BcryptPasswordHasherService } from "../service/BcryptPasswordHasherService";
+
+// Casos de Uso
+import { CreateUserUseCase } from "../../application/useCases/CreateUserUseCase";
+import { GetAllUsersUseCase } from "../../application/useCases/GetAllUsersUseCase";
+import { GetOneUserUseCase } from "../../application/useCases/GetOneUserUseCase";
+import { UpdateUserUseCase } from "../../application/useCases/UpdateUserUseCase";
+import { DeleteUserUseCase } from "../../application/useCases/DeleteUserUseCase";
+
+// Controladores
 import { CreateUserController } from "../controllers/CreateUserController";
 import { GetAllUsersController } from "../controllers/GetAllUsersController";
 import { GetOneUserController } from "../controllers/GetOneUserController";
 import { UpdateUserController } from "../controllers/UpdateUserController";
 import { DeleteUserController } from "../controllers/DeleteUserController";
 
+// Instancias de dependencias
+const userRepository = new PrismaUserRepository();
+const passwordHasherService = new BcryptPasswordHasherService();
+
+const createUserUseCase = new CreateUserUseCase(userRepository, passwordHasherService);
+const getAllUsersUseCase = new GetAllUsersUseCase(userRepository);
+const getOneUserUseCase = new GetOneUserUseCase(userRepository);
+const updateUserUseCase = new UpdateUserUseCase(userRepository);
+const deleteUserUseCase = new DeleteUserUseCase(userRepository);
+
+const createUserController = new CreateUserController(createUserUseCase);
+const getAllUsersController = new GetAllUsersController(getAllUsersUseCase);
+const getOneUserController = new GetOneUserController(getOneUserUseCase);
+const updateUserController = new UpdateUserController(updateUserUseCase);
+const deleteUserController = new DeleteUserController(deleteUserUseCase);
+
 const userRoutes = Router();
 
-const createUser = makeInvoker(CreateUserController);
-const getAllUsers = makeInvoker(GetAllUsersController);
-const getOneUser = makeInvoker(GetOneUserController);
-const updateUser = makeInvoker(UpdateUserController);
-const deleteUser = makeInvoker(DeleteUserController);
-
-userRoutes.post("/", createUser("run"));
-userRoutes.get("/", getAllUsers("run"));
-userRoutes.get("/:id", getOneUser("run"));
-userRoutes.put("/:id", updateUser("run"));
-userRoutes.delete("/:id", deleteUser("run"));
+userRoutes.post("/", createUserController.run.bind(createUserController));
+userRoutes.get("/", getAllUsersController.run.bind(getAllUsersController));
+userRoutes.get("/:id", getOneUserController.run.bind(getOneUserController));
+userRoutes.put("/:id", updateUserController.run.bind(updateUserController));
+userRoutes.delete("/:id", deleteUserController.run.bind(deleteUserController));
 
 export { userRoutes };
