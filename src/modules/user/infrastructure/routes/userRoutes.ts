433d2a208119ab4cdf8a1df853db97bb780f8f34
Victor Raúl García García <victor07mejor@gmail.com>
2026-04-13 23:24:50 -0600

    feat: añade roles y permisos a usuarios

--- src/modules/user/infrastructure/routes/userRoutes.ts
+++ src/modules/user/infrastructure/routes/userRoutes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { container } from "@/core/shared/infrastructure/di/container";
 
+import { authMiddleware } from "@/modules/auth/infrastructure/middlewares/authMiddleware";
+import { roleMiddleware } from "@/modules/auth/infrastructure/middlewares/roleMiddleware";
+
 // Controladores
 import { CreateUserController } from "../controllers/CreateUserController";
 import { GetAllUsersController } from "../controllers/GetAllUsersController";
@@ -17,10 +20,17 @@ const getOneUserController = container.resolve(GetOneUserController);
 const updateUserController = container.resolve(UpdateUserController);
 const deleteUserController = container.resolve(DeleteUserController);
 
-userRoutes.post("/", createUserController.run.bind(createUserController));
-userRoutes.get("/", getAllUsersController.run.bind(getAllUsersController));
-userRoutes.get("/:id", getOneUserController.run.bind(getOneUserController));
-userRoutes.put("/:id", updateUserController.run.bind(updateUserController));
-userRoutes.delete("/:id", deleteUserController.run.bind(deleteUserController));
+// Protegemos todas las rutas requeridas de autenticación
+userRoutes.use(authMiddleware);
+
+// Permisos
+const adminModAccess = roleMiddleware(["ADMIN", "MOD"]);
+const adminOnlyAccess = roleMiddleware(["ADMIN"]);
+
+userRoutes.post("/", adminModAccess, createUserController.run.bind(createUserController));
+userRoutes.get("/", adminModAccess, getAllUsersController.run.bind(getAllUsersController));
+userRoutes.get("/:id", adminModAccess, getOneUserController.run.bind(getOneUserController));
+userRoutes.put("/:id", adminModAccess, updateUserController.run.bind(updateUserController));
+userRoutes.delete("/:id", adminOnlyAccess, deleteUserController.run.bind(deleteUserController));
 
 export { userRoutes };
